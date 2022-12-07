dca160e9f2cf59dab4436348a00f0c7425325265
irinapichalka <irinarescan@gmail.com>
2022-12-07 21:13:02 +0200

    t1

--- react-context/homework/src/UserMenu.jsx
+++ react-context/homework/src/UserMenu.jsx
@@ -5,12 +5,10 @@ class UserMenu extends React.Component {
   render() {
     return (
       <div className="menu">
-        <span className="menu__greeting">
-          {`Hello, ${this.context.userData.name}`}
-        </span>
+        <span className="menu__greeting">{`Hello, ${this.context.name}`}</span>
         <img
           alt="User Avatar"
-          src={this.context.userData.avatar_url}
+          src={this.context.avatar_url}
           className="menu__avatar"
         />
       </div>
