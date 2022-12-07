19acc497b609794a43cd93a3ba98a34b058eb4a8
irinapichalka <irinarescan@gmail.com>
2022-12-08 00:01:41 +0200

    task2

---
+++ lesson2/task2/src/index.js
@@ -0,0 +1,34 @@
+import "./index.scss";
+import store, { increment, decrement, reset } from "./store";
+
+const resultElem = document.querySelector(".counter__result");
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const resetBtn = document.querySelector('[data-action="reset"]');
+
+const onIncrement = () => {
+  store.dispatch(increment());
+};
+
+const onDecrement = () => {
+  store.dispatch(decrement());
+};
+
+const onReset = () => {
+  store.dispatch(reset());
+};
+
+incrementBtn.addEventListener("click", onIncrement);
+decrementBtn.addEventListener("click", onDecrement);
+resetBtn.addEventListener("click", onReset);
+
+store.subscribe(() => {
+  const state = store.getState();
+  const currentValue = state.history.reduce((acc, value) => acc + value, 0);
+  const newArray = state.history.map((el, index) => {
+    return el === 1 && index !== 0 ? "+1" : el;
+  });
+  const historyString = newArray.join("");
+  resultElem.textContent =
+    state.history.length === 0 ? "" : `${historyString} = ${currentValue}`;
+});
