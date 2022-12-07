32338d83e43570c07b06b603ba10c9cff4c898fc
irinapichalka <irinarescan@gmail.com>
2022-12-07 20:48:44 +0200

    homework

--- react-context/homework/src/Header.jsx
+++ react-context/homework/src/Header.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import UserMenu from "./UserMenu";
-import { UserDataContext } from "./userData-context";
 
 const Header = () => {
   return (
     <div className="header">
-      <UserDataContext.Provider value={this.state.userData}>
-        <UserMenu />
-      </UserDataContext.Provider>
+      <UserMenu />
     </div>
   );
 };
--- react-context/homework/src/UserMenu.jsx
+++ react-context/homework/src/UserMenu.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { UserDataContext } from "./userData-context";
 
-const UserMenu = () => {
-  return (
-    <div className="menu">
-      <span className="menu__greeting">
-        Hello, {this.context.userData.name}
-      </span>
-      <img
-        alt="User Avatar"
-        src={this.context.userData.avatar_url}
-        className="menu__avatar"
-      />
-    </div>
-  );
-};
+class UserMenu extends React.Component {
+  render() {
+    return (
+      <div className="menu">
+        <span className="menu__greeting">
+          {`Hello, ${this.context.userData.name}`}
+        </span>
+        <img
+          alt="User Avatar"
+          src={this.context.userData.avatar_url}
+          className="menu__avatar"
+        />
+      </div>
+    );
+  }
+}
 
 UserMenu.contextType = UserDataContext;
 
