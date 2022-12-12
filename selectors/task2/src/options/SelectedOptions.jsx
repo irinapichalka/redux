d437e795c4bc31a7b592dacb62100034bd2727b2
irinapichalka <irinarescan@gmail.com>
2022-12-12 22:01:33 +0200

    t2

---
+++ selectors/task2/src/options/SelectedOptions.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { connect } from "react-redux";
+import { selectedOptionsSelector } from "./options.selectors";
+import { toggleOption } from "./options.actions";
+import Options from "./Options";
+
+const mapState = (state) => {
+  return {
+    options: selectedOptionsSelector(state),
+  };
+};
+
+const mapDispatch = {
+  moveOption: toggleOption,
+};
+
+export default connect(mapState, mapDispatch)(Options);
---
+++ selectors/task2/src/options/options.selectors.js
@@ -0,0 +1,27 @@
+import { createSelector } from "reselect";
+
+export const optionsListSelector = (state) => {
+  return state.options.optionsList;
+};
+
+export const selectedIdsSelector = (state) => {
+  return state.options.selected;
+};
+
+export const selectedOptionsSelector = (state) =>
+  createSelector(
+    [optionsListSelector, selectedIdsSelector],
+    (allOptionsList, selectedIds) => {
+      return allOptionsList.filter((option) => selectedIds.includes(option.id));
+    }
+  );
+
+export const availableOptionsSelector = (state) =>
+  createSelector(
+    [optionsListSelector, selectedIdsSelector],
+    (allOptionsList, selectedIds) => {
+      return allOptionsList.filter(
+        (option) => !selectedIds.includes(option.id)
+      );
+    }
+  );
