9ddaab9807377e7a187266ac2c19362c4a971384
irinapichalka <irinarescan@gmail.com>
2022-12-12 16:57:11 +0200

    bb

--- react/pagination/src/users/Pagination.jsx
+++ react/pagination/src/users/Pagination.jsx
@@ -4,17 +4,21 @@ class Pagination extends React.Component {
   render() {
     const { goPrev, goNext, currentPage, totalItems, itemsPerPage } =
       this.props;
-    const pages = Math.ceil(totalItems / itemsPerPage);
     const isPrevPageAvailable = currentPage === 0;
-    const isNextPageAvailable = pages === currentPage + 1;
+    const isNextPageAvailable =
+      currentPage < Math.ceil(totalItems / itemsPerPage) - 1;
     return (
       <div className="pagination">
         <button className="btn" onClick={goPrev} disabled={isPrevPageAvailable}>
           {!isPrevPageAvailable && "←"}
         </button>
         <span className="pagination__page">{currentPage + 1}</span>
-        <button className="btn" onClick={goNext} disabled={isNextPageAvailable}>
-          {!isNextPageAvailable && "→"}
+        <button
+          className="btn"
+          onClick={goNext}
+          disabled={!isNextPageAvailable}
+        >
+          {isNextPageAvailable && "→"}
         </button>
       </div>
     );
