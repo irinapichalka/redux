51118d793c3a7c0779611fbb68862f8df650b937
irinapichalka <irinarescan@gmail.com>
2022-12-29 16:30:34 +0200

    t2

---
+++ middleware/task2/src/users/users.reducer.js
@@ -0,0 +1,34 @@
+import { SHOW_SPINNER, USER_DATA_RECIEVED } from "./users.actions";
+
+const initialData = {
+  users: {
+    isFetching: false,
+    userData: null,
+  },
+};
+
+const usersReducer = (state = initialData, action) => {
+  switch (action.type) {
+    case SHOW_SPINNER: {
+      return {
+        ...state,
+        users: {
+          isFetching: true,
+        },
+      };
+    }
+    case USER_DATA_RECIEVED: {
+      return {
+        ...state,
+        users: {
+          userData: action.payload,
+          isFetching: false,
+        },
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default usersReducer;
